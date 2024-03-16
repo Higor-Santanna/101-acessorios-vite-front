97c6dcb3515609d6871ded9c855da8e530b037a4
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2024-03-16 15:56:35 -0300

    Configuração do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./routes"
 
 function App() {
   return (
     <>
-      <h1>teste</h1>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <>
+          <h1>Ta funcionado</h1>
+        </>
+    )
+}
+
+
+export { Home }
