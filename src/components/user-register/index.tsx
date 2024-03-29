9a54348531de0a4af450c72bcbec8760881f8c39
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2024-03-29 17:27:31 -0300

    Atualização 29/03/2024 - estilização da pagina de cadastro desktop feita

--- src/components/user-register/index.tsx
+++ src/components/user-register/index.tsx
@@ -35,7 +35,7 @@ const schemaForma = z.object({
 type FormProps = z.infer<typeof schemaForma>
 
 const UserRegister = () => {
-    const [ user, setUser ] = useState(null)
+    const [user, setUser] = useState(null)
 
     const { register, handleSubmit, formState: { errors } } = useForm<FormProps>({
         criteriaMode: 'all',
@@ -63,68 +63,81 @@ const UserRegister = () => {
     return (
         <>
             {user == null ? (
-                <div>
-                    <h2>Novo Cadastro</h2>
-            <form onSubmit={handleSubmit(handleFormSubmit)}>
-                <div>
-                    <label>Nome*</label>
-                    <input {...register('userData.name')} type="text"
-                        placeholder="Nome" required />
-                    {errors.userData?.name?.message && (
-                        <p>Nome inválido</p>
-                    )}
+                <div className="flex
+                flex-col justify-between items-center h-screen w-screen">
+                    <div className="w-full h-20 bg-black"></div>
 
-                    <label>Sobrenome*</label>
-                    <input {...register('userData.lastName')} type="text"
-                        placeholder="Sobrenome" required />
-                    {errors.userData?.lastName?.message && (
-                        <p>SobreNome inválido</p>
-                    )}
-                </div>
-                <br />
-                <div>
-                    <label>CPF*</label>
-                    <input {...register('userData.cpf')} type="number" placeholder="CPF" required maxLength={11} />
-                    {errors.userData?.cpf?.message && (
-                        <p>CPF inválido</p>
-                    )}
+                    <form onSubmit={handleSubmit(handleFormSubmit)} className="flex flex-col items-center w-7/12 max-lg:w-9/12 max-sm:w-10/12 bg-gray-300 rounded py-6 gap-4">
+                        <h2 className="font-semibold text-2xl">Novo Cadastro</h2>
+                        <div className="flex flex-col gap-5">
+                            <div className="flex gap-3">
+                                <div>
+                                    <label className="text-base font-medium pr-1">Nome*</label>
+                                    <input {...register('userData.name')} type="text"
+                                        placeholder="Nome" required className="w-56 rounded border border-black bg-gray-300" />
+                                    {errors.userData?.name?.message && (
+                                        <p className="font-thin text-xs text-red-600 pl-14">Nome inválido</p>
+                                    )}
+                                </div>
+                                <div>
+                                    <label className="text-base font-medium pr-1">Sobrenome*</label>
+                                    <input {...register('userData.lastName')} type="text"
+                                        placeholder="Sobrenome" required className="w-56 rounded border border-black bg-gray-300" />
+                                    {errors.userData?.lastName?.message && (
+                                        <p className="font-thin text-xs text-red-600 pl-24">Sobrenome inválido</p>
+                                    )}
+                                </div>
+                            </div>
+                            <div className="flex gap-3">
+                                <div>
+                                    <label className="text-base font-medium pr-1">CPF*</label>
+                                    <input {...register('userData.cpf')} type="number" placeholder="CPF" required maxLength={11} className="w-56 rounded border border-black bg-gray-300" />
+                                    {errors.userData?.cpf?.message && (
+                                        <p className="font-thin text-xs text-red-600 pl-10">CPF inválido</p>
+                                    )}
 
-                    <label>RG</label>
-                    <input {...register('userData.rg')} type="number" placeholder="RG" maxLength={7} />
-                </div>
-                <br />
-                <div>
-                    <label>Celular*</label>
-                    <input {...register('userData.phoneNumber')} type="number" placeholder="numero do telefone com DDD" required maxLength={11} />
-                    {errors.userData?.phoneNumber?.message && (
-                        <p>Número de telefone inválido</p>
-                    )}
-                </div>
-                <br />
-                <div>
-                    <label>E-mail*</label>
-                    <input {...register('userData.email')} type="email" placeholder="E-mail" required />
-                    {errors.userData?.email?.message && (
-                        <p>Email inválido</p>
-                    )}
-                </div>
-                <br />
-                <div>
-                    <label>Senha*</label>
-                    <input {...register('userData.password')} type="password" placeholder="Senha" required />
-                    {errors.userData?.password?.message && (
-                        <p>Senha inválida</p>
-                    )}
+                                </div>
+                                <div>
+                                    <label className="text-base font-medium pr-1">RG</label>
+                                    <input {...register('userData.rg')} type="number" placeholder="RG" maxLength={7} className="w-56 rounded border border-black bg-gray-300" />
+                                </div>
+                            </div>
 
-                    <label>Confirmar Senha*</label>
-                    <input {...register('userData.confirmPassword')} type="password" placeholder="Confirma Senha" required />
-                    {errors.userData?.confirmPassword?.message && (
-                        <p>Senha inválida</p>
-                    )}
-                </div>
-                <br />
-                <button type="submit">Concluir Cadastro</button>
-            </form>
+                            <div>
+                                <label className="text-base font-medium pr-1">Celular*</label>
+                                <input {...register('userData.phoneNumber')} type="number" placeholder="Número do telefone com DDD" required maxLength={11} className="w-72 rounded border border-black bg-gray-300" />
+                                {errors.userData?.phoneNumber?.message && (
+                                    <p className="font-thin text-xs text-red-600 pl-16">Número de telefone inválido</p>
+                                )}
+                            </div>
+
+                            <div>
+                                <label className="text-base font-medium pr-1">E-mail*</label>
+                                <input {...register('userData.email')} type="email" placeholder="E-mail" required className="w-96 rounded border border-black bg-gray-300" />
+                                {errors.userData?.email?.message && (
+                                    <p className="font-thin text-xs text-red-600 pl-14">Email inválido</p>
+                                )}
+                            </div>
+
+                            <div>
+                                <label className="text-base font-medium pr-1">Senha*</label>
+                                <input {...register('userData.password')} type="password" placeholder="Senha" required className="w-60 rounded border border-black bg-gray-300" />
+                                {errors.userData?.password?.message && (
+                                    <p className="font-thin text-xs text-red-600 pl-14">Senha inválida</p>
+                                )}
+                            </div>
+
+                            <div>
+                                <label className="text-base font-medium pr-1">Confirmar Senha*</label>
+                                <input {...register('userData.confirmPassword')} type="password" placeholder="Confirma Senha" required className="w-60 rounded border border-black bg-gray-300" />
+                                {errors.userData?.confirmPassword?.message && (
+                                    <p className="font-thin text-xs text-red-600 pl-32">Senha inválida</p>
+                                )}
+                            </div>
+                        </div>
+                        <button type="submit" className="w-40 h-8 bg-blue-700 rounded text-white font-medium">Concluir Cadastro</button>
+                    </form>
+                    <div className="w-full h-20 bg-black"></div>
                 </div>
             ) : (
                 <div>
