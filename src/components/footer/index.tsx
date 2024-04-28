3ee6b2ccede48147ad7005d9edd2767470caacc2
Higor Santanna Rodrigues <higorrodrigues44@outlook.com>
2024-04-28 14:10:34 -0300

    Atualização 28/04/2024 - Finalizado a estilização do footer, feito o design para o mobile

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,8 +7,8 @@ import { FaYoutube } from "react-icons/fa6";
 const Footer = () => {
     return (
         <>
-            <footer className=" w-full h-500 bg-background-desktop bg-no-repeat bg-cover flex justify-between items-center font-semibold">
-                <div className="ml-28">
+            <footer className=" w-full h-500 bg-background-desktop bg-no-repeat bg-cover flex justify-between items-center font-semibold max-sm:bg-background-mobile max-sm:h-650 max-sm:flex-col max-sm:justify-center">
+                <div className="ml-28 max-lg:ml-16 max-sm:w-full max-sm:ml-0 max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:px-12">
                     <div className="flex flex-col gap-2 mb-4">
                         <h3 className="text-yellow-300 text-xl">Suporte</h3>
                         <a href="/central-de-atendimento" className="text-base text-white hover:underline">Central de Atendimento</a>
@@ -25,7 +25,7 @@ const Footer = () => {
 
                     <div className="flex flex-col gap-2 mb-4">
                         <h3 className="text-yellow-300 text-xl">Formas de Pagamentos</h3>
-                        <ul className="flex gap-3">
+                        <ul className="flex gap-3 max-lg:flex-wrap">
                             <li className="w-16 h-8 bg-white rounded flex justify-center items-center">
                                 <img src='https://i.postimg.cc/kBTWRZ9G/master-card.png' alt='master-card' className="w-14"/>
                             </li>
@@ -54,7 +54,7 @@ const Footer = () => {
                     </div>
                 </div>
 
-                <div className="mr-28 w-64">
+                <div className="mr-28 w-64 max-lg:mr-16 max-sm:mr-0 max-sm:w-full max-sm:ml-0 max-sm:flex max-sm:flex-col max-sm:justify-center max-sm:px-12">
                     <div className="mb-5">
                         <h3 className="text-yellow-300 text-xl mb-2">Atendimento</h3>
                         <a href="" target="_blank" className="text-base text-white w-64 h-10 bg-green-800 flex justify-center items-center text-lg rounded gap-2">
@@ -67,7 +67,7 @@ const Footer = () => {
                     <p className="text-base text-white">De segunda a sexta, das 08h às 17:30h</p>
                     <p className="text-base text-white">Horário de Brasília</p>
 
-                    <ul className="mt-5 flex justify-between">
+                    <ul className="mt-5 flex justify-between max-sm:w-72">
                         <li className="w-10 h-10 bg-green-800 flex justify-center items-center rounded-full">
                             <a href="" className="text-lg text-white">
                                 <FaInstagram />
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,11 @@ export default {
     extend: {
       backgroundImage: {
         'background-desktop': "url('/src/assets/footer.png')",
+        'background-mobile': "url('/src/assets/background-footer-mobile.png')",
       },
       height: {
         '500': '500px',
+        '650': '650px',
       },
       h3: {
         'font-size': "text-xl",
